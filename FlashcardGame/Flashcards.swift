b5e6dcc8340021ebb0195dee85279d49ad2815dd
iD Student <idstudent@iDs-iMac.local>
2017-07-05 16:52:18 -0700

    data source hooked up to view controller

--- FlashcardGame/Flashcards.swift
+++ FlashcardGame/Flashcards.swift
@@ -11,7 +11,7 @@ import Foundation
 class Flashcard {
     var question : String
     
-    var options [String]
+    var options : [String]
     var correctAnswer : String
     
     init (question : String, options : [String]) {
@@ -21,13 +21,52 @@ class Flashcard {
     }
 }
 
-class cardCollection {
+
+
+
+
+
+
+
+
+
+
+
+class CardCollection {
     
     //Single Instance of the card collection
-    public static var instance : cardCollection = cardCollection()
+    public static var instance : CardCollection = CardCollection()
     
+     //The List of Flashcards
     public private(set) var cards : [Flashcard]
-    public private(set) var currentIndex : Int 
-    private init () {
+    
+    // Which question?
+    public private(set) var currentIndex : Int
+    
+    // Get the current card
+    public var currentCard : Flashcard {
+        get { return cards[currentIndex] }
     }
+    
+    private init () {
+        
+    cards = [ Flashcard(question: "What's the capitol of Washington State?", options:["Olympia", "Tacoma", "Seattle", "Spokane"]),
+              Flashcard(question: "Where is iD Tech's Headquarters?", options:[ "Campbell, CA", "Albequerque, NM", "Bejing, CN"])]
+            
+        currentIndex = 0;
+
+        
+    }
+    
+    public func nextQuestion() {
+    currentIndex += 1
+        if(currentIndex >= cards.count) {
+            currentIndex = 0
+        }
+    }
+    
+    public func checkAnswer(_selectedAnswer : Int) -> Bool {
+        return currentCard.options[_selectedAnswer] == currentCard.correctAnswer
+    }
+    
 }
